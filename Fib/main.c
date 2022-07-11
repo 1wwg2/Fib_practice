33ef0e892b77afa761a034052477d257bcd1ede7
1wwg2 <105918939+1wwg2@users.noreply.github.com>
2022-07-11 14:26:35 +0300

    Func Main, расчет Фиббоначи

--- Fib/main.c
+++ Fib/main.c
@@ -1,8 +1,19 @@
-#include <stdio.h>
-  
-int main(void) {
+int main()
+{
+    int X;
 
-	printf("Hello World");
+    printf("Enter X: ");
+    scanf_s("%d", &X);
 
+    while (X <= 0) {
 
+        printf("Incorrect number, enter  module int type: ");
+        scanf_s("%d", &X);
+
+    }
+    for (int i = 1; i <= X; i++) {
+        printf("%d ", fib(i));
+    }
+
+    return 0;
 }
